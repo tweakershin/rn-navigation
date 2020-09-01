b6b0d2e0381b4704fe67bfa8ab5d1b7e65ec6c06
BeTweak <ys@betweak.com>
2020-09-01 18:05:19 +0900

    no message

---
+++ src/screens/SettingScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Text, View, Button } from 'react-native'
+
+export default class SettingScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button title="로그아웃" onPress={() => {
+          // 로그 아웃 액션
+          // this.props.navigation.navigate('AuthStack', {
+          //   screen: 'Login'
+          // })
+        }} />
+      </View>
+    )
+  }
+}
